f4b19877ac3f59cfa9e2e3ef859f31a368c347a4
MaxFloat <tchandnofame@gmail.com>
2020-11-29 19:31:35 +0330

    skill section finished

--- src/Components/HomePageComponents/Customers.js
+++ src/Components/HomePageComponents/Customers.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import { Container, Card, Row } from 'react-bootstrap';
+
+const myColor = [
+  'blue',
+  'red',
+  'pink',
+  'light-green',
+  'cyan',
+  'white',
+  'orange',
+  'gray',
+];
+const customerList = myColor.map((color) => {
+  return (
+    <Card key={color} className="col-xl-3 col-md-6 m-1">
+      <div style={{ backgroundColor: color, width: '100%', height: '50%' }}>
+        <p>SOME BODY</p>
+      </div>
+      <Card.Body>
+        <Card.Title>SOME BODY</Card.Title>
+        <Card.Text>SOME BODY</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+});
 
 const Customers = () => {
-  return <div>Customers</div>;
+  return (
+    <section>
+      <Container fluid style={{ height: '70vh' }}>
+        <Row
+          className={`d-flex justify-content-center align-items-center`}
+          style={{ height: '70vh' }}
+        >
+          {customerList}
+        </Row>
+      </Container>
+    </section>
+  );
 };
 
 export default Customers;
--- src/Components/HomePageComponents/Skills.js
+++ src/Components/HomePageComponents/Skills.js
@@ -1,23 +1,51 @@
 import React from 'react';
 import { Container, Row } from 'react-bootstrap';
 
+import style from '../Style/Skill.module.css';
+import myFont from '../Style/Font.module.css';
+
+// d-flex justify-content-center align-items-center
+
 const Skills = () => {
   return (
-    <>
-      <Container>
-        <Row>
-          <h1>ویژگی ها</h1>
-          <p>هر آنچه برای دیده شدن نیاز دارید</p>
-        </Row>
-        <Row>
-          <div className="col-12">
-            <img src="../Assets/" alt="کپی رایتینگ" />
+    <Container
+      fluid
+      className={`${style.myContainer}`}
+      style={{ height: '100vh' }}
+    >
+      <Row
+        className="d-flex justify-content-center align-items-center p-5"
+        style={{ height: '100%' }}
+      >
+        <div className={`col-12`} style={{ height: '30%' }}>
+          <h1
+            className={`${myFont.sahelBlack} m-0 p-0 text-center`}
+            style={{ fontSize: '5vw', fontWeight: 'bold' }}
+          >
+            ویژگی ها
+          </h1>
+          <p
+            className={`${myFont.sahelBlack} m-md-2 p-0 text-center`}
+            style={{ fontSize: '3vw', fontWeight: 'bold' }}
+          >
+            هر آنچه برای دیده شدن نیاز دارید
+          </p>
+        </div>
+        {/* <div className={`col-12 mt-0 pt-0`} style={{ height: '50vh' }}> */}
+        <div className="col" style={{ height: '70%' }}>
+          <div className={` bg-white rounded `}>
+            <p className={`${style.myText} text-justify`}>
+              ویژگی آژانس دیجیت آمیختن مهارت های مختلف از جمله توانایی جلب توجه
+              افراد مایل و ترغیب جستجوگران برای دنبال کردن و ارائه خدمات لازم
+              مهارت آراستن ظاهر کسب و کار شما در قالب یک برند و سازمان مطمئن و
+              قابل اطمینان به مشتریان و همراه کردن شبکه های اجتماعی برای محکم
+              کردن تعامل با مشتریان شما{' '}
+            </p>
+            {/* </div> */}
           </div>
-          <div className="col-12"></div>
-          <div className="col-12"></div>
-        </Row>
-      </Container>
-    </>
+        </div>
+      </Row>
+    </Container>
   );
 };
 
